82a69ea77341fdf166fb7e66f4dd7e996d9d99bd
heeewon <rlagmldnjs88@naver.com>
2023-04-08 18:54:56 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -10,12 +10,12 @@ const mainSwiper = new Swiper('.main-swiper', {
   pagination: {
     el: ".pagination",
     type: "custom",
-    renderCustom:function(swiper, current, total){
+    renderCustom: function (swiper, current, total) {
 
-        const currentNum = (current < 10) ? '0'+current : current;
-        const totalNum = (total < 10) ? '0'+total : total;
+      const currentNum = (current < 10) ? '0' + current : current;
+      const totalNum = (total < 10) ? '0' + total : total;
 
-        return `<span class="page-current">${currentNum}</span> 
+      return `<span class="page-current">${currentNum}</span> 
                 <span class="page-total">${totalNum}</span>`;
     }
   },
@@ -23,7 +23,7 @@ const mainSwiper = new Swiper('.main-swiper', {
     nextEl: ".main-swiper .swiper-button-next",
     prevEl: ".main-swiper .swiper-button-prev",
   },
-  });
+});
 
 const bestSwiper = new Swiper(".best-swiper", {
   slidesPerView: 'auto',
@@ -57,12 +57,12 @@ const serviceSwiper = new Swiper('.service-swiper', {
   pagination: {
     el: ".pagination",
     type: "custom",
-    renderCustom:function(swiper, current, total){
+    renderCustom: function (swiper, current, total) {
 
-        const currentNum = (current < 10) ? '0'+current : current;
-        const totalNum = (total < 10) ? '0'+total : total;
+      const currentNum = (current < 10) ? '0' + current : current;
+      const totalNum = (total < 10) ? '0' + total : total;
 
-        return `<span class="page-current">${currentNum}</span> 
+      return `<span class="page-current">${currentNum}</span> 
                 <span class="page-total">${totalNum}</span>`;
     }
   },
@@ -80,7 +80,6 @@ const reviewSwiper = new Swiper(".review-swiper", {
 const footerBanner = new Swiper('.footer-banner', {
   slidesPerView: 'auto',
   spaceBetween: 20,
-  centeredSlides: true,
   autoplay: {
     delay: 0,
     disableOnInteraction: false,
@@ -94,25 +93,25 @@ const footerBanner = new Swiper('.footer-banner', {
 //스와이퍼 제어 버튼
 const mainControl = $('.sc-banner .btn-control');
 
-mainControl.click(function(){
-  if($(this).hasClass('on')){
-      $(this).removeClass('on');
-      mainSwiper.autoplay.start();
-  }else{
-      $(this).addClass('on');
-      mainSwiper.autoplay.stop();
+mainControl.click(function () {
+  if ($(this).hasClass('on')) {
+    $(this).removeClass('on');
+    mainSwiper.autoplay.start();
+  } else {
+    $(this).addClass('on');
+    mainSwiper.autoplay.stop();
   }
 });
 
 const eventControl = $('.sc-event .btn-control');
 
-eventControl.click(function(){
-  if($(this).hasClass('on')){
-      $(this).removeClass('on');
-      eventSwiper.autoplay.start();
-  }else{
-      $(this).addClass('on');
-      eventSwiper.autoplay.stop();
+eventControl.click(function () {
+  if ($(this).hasClass('on')) {
+    $(this).removeClass('on');
+    eventSwiper.autoplay.start();
+  } else {
+    $(this).addClass('on');
+    eventSwiper.autoplay.stop();
   }
 });
 
@@ -124,17 +123,19 @@ gsap.registerPlugin(ScrollTrigger);
 //section 슬라이드 업(공통)
 const sectionEl = document.querySelectorAll('section.up')
 
-sectionEl.forEach(function(l) {
-  gsap.set('section.up', {y:130});
+sectionEl.forEach(function (l) {
+  gsap.set('section.up', { y: 130 });
 
   const contentUp = gsap.timeline(
-    {scrollTrigger: {
-      trigger: l,
-      start:"top 80%",
-      end:"10% 88%",
-      scrub: 2,
-  }});
-  contentUp.to(l, {y:0})
+    {
+      scrollTrigger: {
+        trigger: l,
+        start: "top 80%",
+        end: "10% 88%",
+        scrub: 2,
+      }
+    });
+  contentUp.to(l, { y: 0 })
 });
 
 
@@ -144,33 +145,33 @@ let lastScrollTop = 0;
 const delta = 5;
 const navbarHeight = $('.header').outerHeight();
 
-$(window).scroll(function(event){
-    didScroll = true;
+$(window).scroll(function (event) {
+  didScroll = true;
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
 function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.header').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.header').removeClass('nav-up')
-        }
+  var st = $(this).scrollTop();
+
+  if (Math.abs(lastScrollTop - st) <= delta)
+    return;
+
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $('.header').addClass('nav-up');
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $('.header').removeClass('nav-up')
     }
-    lastScrollTop = st;
+  }
+  lastScrollTop = st;
 }
 
 
@@ -180,17 +181,17 @@ const menuClose = document.querySelector('.menu-inner .close-btn');
 const body = document.getElementsByTagName('body')[0];
 const menuBg = document.querySelector('.menu-bg')
 
-menuOpen.addEventListener('click',function(e){
+menuOpen.addEventListener('click', function (e) {
   e.preventDefault();
   body.classList.add('scrollLock');
 });
 
-menuClose.addEventListener('click', function(e){
+menuClose.addEventListener('click', function (e) {
   e.preventDefault();
   body.classList.remove('scrollLock');
 });
 
-menuBg.addEventListener('click', function(e){
+menuBg.addEventListener('click', function (e) {
   e.preventDefault();
   body.classList.remove('scrollLock');
 });
@@ -199,11 +200,11 @@ menuBg.addEventListener('click', function(e){
 //메뉴창 서브 메뉴 열고 닫기
 const subMenu = $('.menu-inner .cate-item');
 
-subMenu.click(function(e){
+subMenu.click(function (e) {
   e.preventDefault();
-  if($(this).hasClass('on')){
+  if ($(this).hasClass('on')) {
     $(this).removeClass('on');
-  }else{
+  } else {
     $(this).addClass('on').siblings().removeClass('on');
   }
 });
@@ -215,20 +216,20 @@ const opList = document.querySelector('.sc-pick .op-list');
 
 
 //sc-pick 리스트 열고 닫기
-opTitle.addEventListener('click',function(){
+opTitle.addEventListener('click', function () {
   opList.classList.toggle('on')
 });
 
 
 //sc-pick 리스트 선택 이벤트
-opList.addEventListener('click', function(event){
-  if(event.target.tagName !== 'BUTTON') return false;
+opList.addEventListener('click', function (event) {
+  if (event.target.tagName !== 'BUTTON') return false;
   opTitle.innerText = event.target.innerText;
   opList.classList.remove('on');
 });
 
 
 //footer toggle
-$(".familysite").click(function(){
+$(".familysite").click(function () {
   $(".site-list").slideToggle(200);
 })
